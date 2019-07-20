a669390c562cfcc36dec8b85a86b99e1265912f7
Michael Fox <michael.c.fox.ii@gmail.com>
2019-07-20 12:18:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,8 +46,15 @@ class Dog
   end
   
     def self.find_or_create_by(name:, breed:)
+      sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE name = ?
+        AND breed = ?
+        LIMIT 1 
+      SQL
     
-     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?, name, breed")
+     dog = DB[:conn].execute(sql, name, breed)
     
      if !dog.empty?
        dog_data = dog[0]
