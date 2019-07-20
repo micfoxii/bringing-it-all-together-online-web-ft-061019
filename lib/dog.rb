f2ecfab9bbed1ee0bcb667a7cff9dc3320d6f33c
Michael Fox <michael.c.fox.ii@gmail.com>
2019-07-20 11:48:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,6 +24,29 @@ class Dog
     DB[:conn].execute(sql)
   end
   
+  def save
+    if self.id
+      self.update
+    else 
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
+  
+      
+      
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+  
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
